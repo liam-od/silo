d01a511733463a88e3d60d0f4d8d8ba1822863f6
liamod <liamgregoryod@gmail.com>
2026-09-17 19:45:33 +0200

    feat: list Silo-managed VMs

--- cmd/silo/main.go
+++ cmd/silo/main.go
@@ -5,27 +5,59 @@ import (
 	"os"
 
 	incus "github.com/lxc/incus/v6/client"
+	"github.com/lxc/incus/v6/shared/api"
 )
 
 func main() {
-	if err := run(); err != nil {
+	if err := run(os.Args[1:]); err != nil {
 		fmt.Fprintf(os.Stderr, "silo: %v\n", err)
 		os.Exit(1)
 	}
 }
 
-func run() error {
+func run(args []string) error {
+	if len(args) > 1 {
+		return fmt.Errorf("too many arguments")
+	}
+
+	command := ""
+	if len(args) == 1 {
+		command = args[0]
+	}
+
+	if command != "" && command != "list" {
+		return fmt.Errorf("unknown command %q", command)
+	}
+
 	client, err := incus.ConnectIncusUnix("", nil)
 	if err != nil {
 		return fmt.Errorf("connect to Incus: %w", err)
 	}
-	server, _, err := client.GetServer()
-	if err != nil {
-		return fmt.Errorf("get Incus server information: %w", err)
+
+	switch command {
+	case "":
+		server, _, err := client.GetServer()
+		if err != nil {
+			return fmt.Errorf("get Incus server information: %w", err)
+		}
+
+		fmt.Printf("Connected to Incus %s\n", server.Environment.ServerVersion)
+		fmt.Printf("Project: %s\n", server.Environment.Project)
+	case "list":
+		instances, err := client.GetInstances(api.InstanceTypeVM)
+		if err != nil {
+			return fmt.Errorf("get VM instances: %w", err)
+		}
+		found := false
+		for _, instance := range instances {
+			if instance.Config["user.silo.managed"] == "true" {
+				fmt.Printf("%s: %s\n", instance.Name, instance.Status)
+				found = true
+			}
+		}
+		if !found {
+			fmt.Println("No Silo-managed VMs.")
+		}
 	}
-
-	fmt.Printf("Connected to Incus %s\n", server.Environment.ServerVersion)
-	fmt.Printf("Project: %s\n", server.Environment.Project)
-
 	return nil
 }
